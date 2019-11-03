64c63dc7269ad6246797e941571ab6d9b39fa8f6
slashflex <pro.davidsaoud@gmail.com>
2019-11-03 15:04:25 +0100

    Roll + hold dice & scores updated

--- app.js
+++ app.js
@@ -8,3 +8,70 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener("click", function () {
+    
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update roundScore IF the rolled dice was not a 1
+    if (dice !== 1) {
+
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+
+        // Next Player
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+
+    // 1. Add current score to Global score
+    scores[activePlayer] += roundScore;
+
+    // 2. Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Next Player
+    nextPlayer();
+
+    // 3. Check if player won the game
+});
+
+
+function nextPlayer() {
+    // Next Player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
